a4f4d596ac36d893eb0b63e76d537f2ba1540fb8
MarcosMatias2602 <marcos.matias26@gmail.com>
2025-04-01 22:09:55 +0000

    scanf e printf carta1

--- xadrez.c
+++ xadrez.c
@@ -1,16 +1,24 @@
-#include <stdio.h>;
+#include <stdio.h>
 
 int main() {
 
     //carta1
     
-    char nome [30] = "Recife";
-    int populacao = 1000000;
-    float area = 1098.25;
-    int pontos = 50;
+    char nome1 [30] = "Recife";
+    int populacao1 = 1000000;
+    float area1 = 1098.25;
+    int pontos1 = 50;
 
+    //carta2
 
-
+    char nome2 [30] = "Caruaru";
+    int populacao2 = 500000;
+    float area2 = 1100.00;
+    int pontos2 = 30;
+    
+    printf("Qual a população da cidade1:");
+    scanf("&d", populacao1);
+    printf("A população da cidade1 é: %d\n", populacao1);
 
 
 }
