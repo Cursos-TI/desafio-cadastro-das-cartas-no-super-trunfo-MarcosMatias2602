607f60faf804464ac2917840b42d2a33309d3943
MarcosMatias2602 <marcos.matias26@gmail.com>
2025-04-01 23:48:25 +0000

    carta1 criada

--- xadrez.c
+++ xadrez.c
@@ -2,7 +2,7 @@
 
 int main() {
 
-    //carta1
+    //Carta1
     
     char nome1 [30] = "Recife";
     int populacao1 = 1000000;
@@ -16,9 +16,24 @@ int main() {
     float area2 = 1100.00;
     int pontos2 = 30;
     
+    printf("Qual o nome da cidade1:");
+    scanf("%s", &nome1);
+    
     printf("Qual a população da cidade1:");
-    scanf("&d", populacao1);
-    printf("A população da cidade1 é: %d\n", populacao1);
+    scanf("%d", &populacao1);
+
+    printf("Qual a area da cidade1:");
+    scanf("%f", &area1);
+
+    printf("Quantos pontos turísticos tem a cidade1:");
+    scanf("%d", &pontos1);
+
+    printf("A seguir temos os dados da Carta1:\n");
+    printf("O nome da cidade1 é: %s\n", nome1);
+    printf("A população da cidade1: %d\n", populacao1);
+    printf("A area da cidade1 é: %f\n", area1);
+    printf("A quantidade de pontos turísticos da cidade1 é: %d\n", pontos1);
+
 
 
 }
