6cddc705e1460a73dfda9afec82570a9fc6c0753
MarcosMatias2602 <marcos.matias26@gmail.com>
2025-04-01 01:04:56 +0000

    criei uma carta

---
+++ xadrez.c
@@ -0,0 +1,16 @@
+#include <stdio.h>;
+
+int main() {
+
+    //carta1
+    
+    char nome [30] = "Recife";
+    int populacao = 1000000;
+    float area = 1098.25;
+    int pontos = 50;
+
+
+
+
+
+}
